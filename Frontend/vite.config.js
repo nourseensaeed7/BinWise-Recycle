9df466537730cb396121d711961973a3d276c3e8
Nourseen Saeed <nourseensaeed7@gmail.com>
2025-12-01 04:45:51 +0200

    vite

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,16 +1,22 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000",
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Use '.' instead of process.cwd()
+  const env = loadEnv(mode, '.', '');
+  
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+  };
 });
